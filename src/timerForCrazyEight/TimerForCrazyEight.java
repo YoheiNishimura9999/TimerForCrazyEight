c45097628774b40cb111f1955920f6f69700103a
YoheiNishimura <nishimura@michael-inc.com>
2019-12-24 12:58:16 +0900

    complete

--- src/timerForCrazyEight/TimerForCrazyEight.java
+++ src/timerForCrazyEight/TimerForCrazyEight.java
@@ -18,13 +18,15 @@ public class TimerForCrazyEight extends Frame implements ActionListener{
 	private final static int WIDTH  = 500;
 	private final static int HEIGHT = 500;
 	private Label l = new Label("30");
-	private Label taskL = new Label("Task track 0");
-	private Button bt = new Button("タスクトラック");
+	private Label taskL = new Label("Timer for Crazy Eight!");
+	private Button bt = new Button("タスクスタート");
 	private Button btS = new Button("ストップ");
 	public int count_timer = 0;
 	public int iteration = 0;
 	public int track = 0;
 	public Timer timer = null;
+
+	private int MAX_track = 3;
 
 	//constructor
 	public TimerForCrazyEight() {
@@ -61,6 +63,8 @@ public class TimerForCrazyEight extends Frame implements ActionListener{
 	// method
 	public void actionPerformed(ActionEvent ev){
 		if(ev.getSource() == bt){
+				iteration = 0;
+				track = 0;
 				timer = new java.util.Timer(false);
 				timer.schedule(new MyTimeTask(), 1000l, 1000l);
 		}
@@ -84,13 +88,23 @@ public class TimerForCrazyEight extends Frame implements ActionListener{
 				timer.cancel();
 				timer = new java.util.Timer(false);
 				timer.schedule(new MyTimeTask(), 1000l, 1000l);
+				if(track >= MAX_track) {
+					l.setText("Finish!");
+					taskL.setText("Finish!");
+					timer.cancel();
+					timer = null;
+					btS.setEnabled(false);
+				}
 				if(iteration%2 == 0){
 					taskL.setText("Track iteration is "+ Integer.toString( track+1 ));
 					count_timer = 31;
 					iteration++;
 					track++;
+					System.out.println(track);
 				} else {
-					taskL.setText("Rest!");
+					if (track < MAX_track) {
+						taskL.setText("Rest!");
+					}
 					count_timer = 11;
 					iteration++;
 				}
