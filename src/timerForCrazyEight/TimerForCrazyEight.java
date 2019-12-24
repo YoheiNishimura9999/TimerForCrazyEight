6788df81c26b5c95525eb1715a9c9a7429032201
YoheiNishimura <nishimura@michael-inc.com>
2019-12-24 14:15:46 +0900

    音を出すなど

--- src/timerForCrazyEight/TimerForCrazyEight.java
+++ src/timerForCrazyEight/TimerForCrazyEight.java
@@ -1,6 +1,7 @@
 package timerForCrazyEight;
 
 import java.awt.Button;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Frame;
 import java.awt.GridLayout;
@@ -9,8 +10,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class TimerForCrazyEight extends Frame implements ActionListener{
 
@@ -19,12 +31,17 @@ public class TimerForCrazyEight extends Frame implements ActionListener{
 	private final static int HEIGHT = 500;
 	private Label l = new Label("30");
 	private Label taskL = new Label("Timer for Crazy Eight!");
-	private Button bt = new Button("タスクスタート");
-	private Button btS = new Button("ストップ");
-	public int count_timer = 0;
-	public int iteration = 0;
-	public int track = 0;
-	public Timer timer = null;
+	private Button bt = new Button("Start!");
+	private Button btS = new Button("Stop");
+	private int count_timer = 0;
+	private int iteration = 0;
+	private int track = 0;
+	private int TASK_TIME = 30;
+	private int REST_TIME = 10;
+	private Timer timer = null;
+
+	private String soundFile = "./src/timerForCrazyEight/bip.wav";
+	private Clip clip;
 
 	private int MAX_track = 8;
 
@@ -39,13 +56,15 @@ public class TimerForCrazyEight extends Frame implements ActionListener{
 			}
 		);
 
-		setTitle("Timer for Crazy Eight!");		//タイトルを設定
+		setTitle("Timer for Crazy Eight!");
 		setLayout(new GridLayout(4,1));
 		l.setAlignment(Label.CENTER);
 		l.setFont(new Font("Arial", Font.PLAIN, 96));
 		taskL.setFont(new Font("Arial", Font.PLAIN,48));
 		add(taskL);
 		add(l);
+		bt.setFont(new Font("Arial", Font.BOLD, 24));
+		btS.setFont(new Font("Arial", Font.BOLD, 24));
 		add(bt);
 		add(btS);
 		btS.setEnabled(false);
@@ -83,11 +102,20 @@ public class TimerForCrazyEight extends Frame implements ActionListener{
 			if (count_timer > 0) {
 				count_timer --;
 				l.setText(Integer.toString(count_timer));
+				if(count_timer < 6){
+					int fontNum = 96+(50-10*count_timer);
+					l.setFont(new Font("Arial", Font.BOLD, fontNum));
+					l.setForeground(Color.RED);
+					clip = createClip(new File(soundFile));
+					clip.start();
+				}
 				btS.setEnabled(true);
 			} else {
 				timer.cancel();
 				timer = new java.util.Timer(false);
 				timer.schedule(new MyTimeTask(), 1000l, 1000l);
+				l.setFont(new Font("Arial", Font.PLAIN, 96));
+				l.setForeground(Color.BLACK);
 				if(track >= MAX_track) {
 					l.setText("Finish!");
 					taskL.setText("Finish!");
@@ -97,7 +125,7 @@ public class TimerForCrazyEight extends Frame implements ActionListener{
 				}
 				if(iteration%2 == 0){
 					taskL.setText("Track iteration is "+ Integer.toString( track+1 ));
-					count_timer = 31;
+					count_timer = TASK_TIME;
 					iteration++;
 					track++;
 					System.out.println(track);
@@ -105,12 +133,42 @@ public class TimerForCrazyEight extends Frame implements ActionListener{
 					if (track < MAX_track) {
 						taskL.setText("Rest!");
 					}
-					count_timer = 11;
+					count_timer = REST_TIME;
 					iteration++;
 				}
 
 			}
 		}
 	}  // end of MyTimeTask
+
+	public static Clip createClip(File path) {
+		//指定されたURLのオーディオ入力ストリームを取得
+		try (AudioInputStream ais = AudioSystem.getAudioInputStream(path)){
+
+			//ファイルの形式取得
+			AudioFormat af = ais.getFormat();
+
+			//単一のオーディオ形式を含む指定した情報からデータラインの情報オブジェクトを構築
+			DataLine.Info dataLine = new DataLine.Info(Clip.class,af);
+
+			//指定された Line.Info オブジェクトの記述に一致するラインを取得
+			Clip c = (Clip)AudioSystem.getLine(dataLine);
+
+			//再生準備完了
+			c.open(ais);
+
+			return c;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 
 }//  end of class
